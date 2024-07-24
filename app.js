9d0e4db114205695161722ac1c9c41b3aea7969e
Alexandre Augusto <alexandreagrg82@gmail.com>
2024-07-24 12:24:43 -0300

    novo teste terminal

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 //isto é outro teste
 //testando a integração git no vs code
 //testando o uso do terminal
+//novo teste terminal
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
