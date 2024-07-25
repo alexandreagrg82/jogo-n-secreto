ecf946a4f39b69370f78902dffb3f732ab8822b1
Alexandre Augusto <alexandreagrg82@gmail.com>
2024-07-25 10:14:04 -0300

    Reapply "removendo mensagem linha 11"
    This reverts commit 5c6376d9be7f7c728872535a0720c520f0193418.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function exibirTextoNaTela(tag, texto) {
 //testanto revert
 
 
+
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
